bac5830a5d21df3afebbf96cf9b480b8c12092fb
Takashi-1027 <taka19921027@gmail.com>
2021-04-09 11:59:34 +0000

    [add]Task controller and View

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "homes#top"
+  get 'home/about' => 'homes#about'
+  
+  resources :users
+  resources :tasks
+  
 end

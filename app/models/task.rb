8c8169f3d068c836f43ba24b1b590cdbe14196ce
Takashi-1027 <taka19921027@gmail.com>
2021-04-10 14:44:37 +0000

    [update] Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :label_maps, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+
+  # 優先ステータス
+  enum priority: {最優先: 0, 優先: 1, 準優先: 2 , 低優先: 3}
+
+  # タスクの進捗ステータス
+  enum status: {未着手: 0, 着手中: 1, 保留: 2 , 遅れ: 3 , 完了: 4}
 end

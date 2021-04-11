7dac1988941876b1d0782b783d5cb6120e889805
Takashi-1027 <taka19921027@gmail.com>
2021-04-11 15:18:12 +0000

    [update] ユーザー一覧・詳細・編集機能と画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,20 @@ class UsersController < ApplicationController
   # ログイン済ユーザーのみにアクセスを許可する
   before_action :authenticate_user!
   # ログインしているユーザーでしか編集できないようにする
-  before_action :ensure_correct_user
-  
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -24,17 +24,22 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :introduction, :profile_image)
   end
 
+  # ログインしているユーザーが他のユーザーと同一人物か確認するコード
   def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to books_path　
+    unless User.find_by(id: params[:id]).nil?
+      @user = User.find(params[:id])
+      unless @user == current_user
+        redirect_to users_path
+      end
+    else
+      redirect_to users_path
     end
   end
 end

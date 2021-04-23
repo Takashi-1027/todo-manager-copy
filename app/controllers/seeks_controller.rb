a8846543f675d4fb564897793fcccca60ccaa61a
Takashi-1027 <taka19921027@gmail.com>
2021-04-23 15:08:04 +0000

    [fix] seek

--- app/controllers/seeks_controller.rb
+++ app/controllers/seeks_controller.rb
@@ -6,15 +6,16 @@ class SeeksController < ApplicationController
 
       # タイトルの検索
       if @seek == "perfect_much"
-        @tasks = Task.all
-        
+        # @tasks = Task.all
+        @tasks = current_user.tasks
+
         # 一つのフォームに複数の検索をかけるときに、次のソースのような書き方ができる。
         # Task.where(title: @word).or(priority: @word).or(status: @word)
-        
+
         if @word.present?   #もし@wordが何もなかったら（完全一致）という意味になる。
           @tasks = @tasks.where("title like?", "#{@word}")
         end
-        
+
       elsif @seek  == "forward_mach"
         @tasks = Task.where("title like?", "#{@word}%")
       elsif @seek  == "backward_mach"

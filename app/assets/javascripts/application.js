e9317703dca6fb70d31a78952f3f7bb635d8dd2a
Takashi-1027 <taka19921027@gmail.com>
2021-04-23 13:24:53 +0000

    [fix] layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,10 +41,21 @@ $(document).ready(function () {
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 2000,
+    autoPlayDuration : 3000,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
   });
 });
+
+//------------------------scrollreveal------------------------//
+$(function(){
+ScrollReveal().reveal('.text', {
+  reset: true,
+  duration: 1600,
+  scale: 4,
+      }
+    );
+  }
+);

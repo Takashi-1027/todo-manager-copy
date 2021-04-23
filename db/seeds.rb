4ffc45be5ed85d13fc45231994f416bd6bd1c152
Takashi-1027 <taka19921027@gmail.com>
2021-04-23 16:24:16 +0000

    [fix] stylesheets, views/users, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,61 @@
 #   Character.create(name: 'Luke', movie: movies.first)
  User.create!(
   email: "tttttt@test.com",
-  name: "テスタロウ",
+  name: "坂蒔 虹真",
   introduction: "がんばるぞい！",
   password: "999999"
   )
-
-  5.times do |n|
-    User.create!(
-      email: "test#{n + 1}@test.com",
-      name: "赤羽#{n + 1}",
-      introduction: "よろしくね！#{n + 1}",
-      # profile_image_id: File.open('./app/assets/images/stil-vdaJJbls3xE-unsplash.jpg'),
-      password: "password#{n + 1}"
-    )
-  end
+ User.create!(
+  email: "FcA2b6@test.com",
+  name: "大石 正樹",
+  introduction: "がんばるぞい！",
+  password: "p498nm8a"
+  )
+ User.create!(
+  email: "4Fmddh@test.com",
+  name: "神田 恭斗",
+  introduction: "がんばるぞい！",
+  password: "g6DR08Yb"
+  )
+ User.create!(
+  email: "B508F8@test.com",
+  name: "琉子 慶峰",
+  introduction: "がんばるぞい！",
+  password: "zIwHKn2x"
+  )
+ User.create!(
+  email: "rQThu3@test.com",
+  name: "嘉数 堺李",
+  introduction: "がんばるぞい！",
+  password: "2PMK74xA"
+  )
+ User.create!(
+  email: "vpIGi9@test.com",
+  name: "内藤 英水",
+  introduction: "がんばるぞい！",
+  password: "wH6xzT7U"
+  )
+ User.create!(
+  email: "7FRS7c@test.com",
+  name: "大舘 凪斗",
+  introduction: "がんばるぞい！",
+  password: "Sh5h0U8T"
+  )
+ User.create!(
+  email: "4oz4lD@test.com",
+  name: "野村 竜丙",
+  introduction: "がんばるぞい！",
+  password: "5Xu8pNYd"
+  )
+ User.create!(
+  email: "Qp498o@test.com",
+  name: "前川 時休",
+  introduction: "がんばるぞい！",
+  password: "q4B62M1B"
+  )
+ User.create!(
+  email: "r6xX8m@test.com",
+  name: "小川 猛",
+  introduction: "がんばるぞい！",
+  password: "igI0URYD"
+  )
